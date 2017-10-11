f0bfa2b54c36dcdf68c08dd6c03db6fa271a4b39
Guest User <Guest@Epicodus-4B.local>
2017-10-11 14:09:06 -0700

    add logic for fragment dialog.

--- app/src/main/java/com/example/guest/dialogfragmentproject/MainActivity.java
+++ app/src/main/java/com/example/guest/dialogfragmentproject/MainActivity.java
@@ -2,18 +2,44 @@ package com.example.guest.dialogfragmentproject;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import android.content.Intent;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 
 public class MainActivity extends AppCompatActivity {
-    FragmentManager fm = getSupportFragmentManager();
+@Bind(R.id.moodButton)
+Button mMoodButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MoodDialogFragment mdf = new MoodDialogFragment();
-        mdf.show(fm, "Sample Fragment");
+        ButterKnife.bind(this);
+
+        mMoodButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                    FragmentManager fm = getSupportFragmentManager();
+                    MoodDialogFragment moodDialogFragment = new MoodDialogFragment();
+                    moodDialogFragment.show(fm, "thing");
+            }
+        });
+
     }
-
-
 }
--- app/src/main/java/com/example/guest/dialogfragmentproject/MoodDialogFragment.java
+++ app/src/main/java/com/example/guest/dialogfragmentproject/MoodDialogFragment.java
@@ -1,22 +1,36 @@
 package com.example.guest.dialogfragmentproject;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.guest.dialogfragmentproject.R;
 
-public class MoodDialogFragment extends DialogFragment{
+public class MoodDialogFragment extends DialogFragment {
 
-    @Override
+   @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_mood_dialog, container, false);
-        getDialog().setTitle("Simple Dialog");
-        return rootView;
-    }
 
+       View rootView = inflater.inflate(R.layout.fragment_mood_dialog, container, false);
 
+       Button cancelButton = (Button) rootView.findViewById(R.id.cancelButton);
+       Button submitButton = (Button) rootView.findViewById(R.id.submitButton);
+
+       cancelButton.setOnClickListener(new View.OnClickListener() {
+
+           @Override
+           public void onClick(View v) {
+               dismiss();
+           }
+       });
+       return rootView;
+
+   }
 }
